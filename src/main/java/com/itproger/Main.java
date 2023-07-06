cfb8a5a7a5b08587038e41a346170e0f33a5b2ab
Islam-Kaziev <kaziev19691@gmail.com>
2023-07-06 23:28:58 +0300

    Finished homework number two

---
+++ src/main/java/com/itproger/Main.java
@@ -0,0 +1,10 @@
+package com.itproger;
+
+public class Main {
+    public static void main(String[] args) {
+        Player bot = new Player();
+        Player alex = new Player(VARIANTS.SCISSORS, "Alex");
+
+        System.out.println(bot.whoWins(bot, alex));
+        }
+    }
